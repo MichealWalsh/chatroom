243c0a29ad0556cf7650ba6e10261f6423ffd884
MichealWalsh <a00226308@student.ait.ie>
2017-07-25 22:06:47 +0100

    Setup basic Maven structure

---
+++ src/main/java/com/br0grammers/chatroom/App.java
@@ -0,0 +1,13 @@
+package com.br0grammers.chatroom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
